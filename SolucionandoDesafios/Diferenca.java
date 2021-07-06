084297ac2738edab98319a840b092dafbcab5b03
vitor.prata@me.com <vitor.prata@me.com>
2021-07-06 17:04:13 -0600

    Primeiro desafio feito com sucesso!

---
+++ SolucionandoDesafios/Diferenca.java
@@ -0,0 +1,21 @@
+package SolucionandoDesafios;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Diferenca {
+
+    public static void main(String[] args) throws IOException {
+        Scanner leitor = new Scanner(System.in);
+        int a = leitor.nextInt();
+        int b = leitor.nextInt();
+        int c = leitor.nextInt();
+        int d = leitor.nextInt();
+      
+       int resultado = (a*b) - (c*d);
+       
+        System.out.println("DIFERENCA ="+resultado);
+        leitor.close();	
+    }
+    
+}
